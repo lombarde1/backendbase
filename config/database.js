7ccefcf5ca1bd0d4bbf2c123b1ad53b3832d3c3c
lombarde1 <milenakurst@gmail.com>
2025-02-17 16:22:48 -0300

    url

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://darkvips:lombarde1@147.79.111.143:27017/coin_principal', {
+    await mongoose.connect('mongodb://darkvips:lombarde1@147.79.111.143:27017/picoinvest1', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       authSource: 'admin'
